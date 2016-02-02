371dc0dfdcfaa865a986c88d24dec5465e0206c7
Anton <anton.poltoratskii@gmail.com>
2016-02-02 20:47:23 +0200

    add some changes to NavigationDrawer

--- app/src/main/java/com/polant/touristapp/drawer/NavigationDrawer.java
+++ app/src/main/java/com/polant/touristapp/drawer/NavigationDrawer.java
@@ -10,6 +10,7 @@ import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.polant.touristapp.Constants;
@@ -47,17 +48,16 @@ public class NavigationDrawer {
                         new PrimaryDrawerItem().withName("Карта").withIcon(R.drawable.ic_map).withIdentifier(1),
                         new PrimaryDrawerItem().withName("Поиск").withIcon(R.drawable.ic_magnify).withIdentifier(2),
                         new PrimaryDrawerItem().withName("Метки").withIcon(R.drawable.ic_bookmark).withIdentifier(3),
+                        new DividerDrawerItem(),
                         new PrimaryDrawerItem().withName("Настройки").withIcon(R.drawable.ic_settings).withIdentifier(4),
-                        new PrimaryDrawerItem().withName("Помощь").withIcon(R.drawable.ic_help).withIdentifier(5)
-                )
-                .addStickyDrawerItems(
+                        new PrimaryDrawerItem().withName("Помощь").withIcon(R.drawable.ic_help).withIdentifier(5),
                         new PrimaryDrawerItem().withName("Обратная связь").withIcon(R.drawable.ic_contact_mail).withIdentifier(6)
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         //TODO: реализовать обработку клика по navigation drawer.
-                        switch (position){
+                        switch (position) {
                             case 2://Поиск.
                                 Intent search = new Intent(activity, SearchActivity.class);
                                 search.putExtra(Constants.USER_ID, Constants.DEFAULT_USER_ID_VALUE);
@@ -77,7 +77,7 @@ public class NavigationDrawer {
                         return false;
                     }
                 })
-                .withSelectedItem(-1)
+                .withSelectedItem(1)
                 .build();
 
         //Анимация проворота иконки при клике на нее для вызова drawer-а.
--- app/src/main/java/com/polant/touristapp/fab/ScrollAwareFABBehavior.java
+++ app/src/main/java/com/polant/touristapp/fab/ScrollAwareFABBehavior.java
@@ -38,5 +38,4 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
             child.show();
         }
     }
-
 }
