fa679989d25338c0e4262e2abab67538f0cec13f
Anton <anton.poltoratskii@gmail.com>
2016-02-04 14:01:39 +0200

    add page scrolling listening

--- app/src/main/java/com/polant/touristapp/activity/HelpActivity.java
+++ app/src/main/java/com/polant/touristapp/activity/HelpActivity.java
@@ -6,11 +6,13 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.polant.touristapp.R;
 import com.polant.touristapp.adapter.tabs.TabsHelpFragmentAdapter;
-import com.polant.touristapp.fragment.TabHelpFragment;
+import com.polant.touristapp.fragment.base.tabs.BaseTabFragment;
 
 /**
  * Created by Антон on 03.02.2016.
@@ -23,14 +25,23 @@ public class HelpActivity extends AppCompatActivity {
 
     private TextView mHelpText;
 
+    private ViewGroup mViewPagerRoot;
+    private View mFooterView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
 
-        mHelpText = (TextView)findViewById(R.id.helpTextView);
+        initViews();
         initToolbar();
         initTabLayout();
+    }
+
+    private void initViews() {
+        mHelpText = (TextView)findViewById(R.id.helpTextView);
+        mViewPagerRoot = (ViewGroup)findViewById(R.id.layout_view_pager_root);
+        mFooterView = findViewById(R.id.layout_footer_help);
     }
 
     private void initToolbar() {
@@ -65,14 +76,45 @@ public class HelpActivity extends AppCompatActivity {
 
     private class PagerChangeListener implements ViewPager.OnPageChangeListener {
 
+        private int footerHeight = 0;
+
+        private final int positionFeedback = 5;
+
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            if (position == positionFeedback - 1){
+                if (footerHeight <= 0){
+                    footerHeight = mFooterView.getHeight();
+                }
+                if (footerHeight > 0) {
+                    int footerTranslation = (int) (2 * footerHeight * positionOffset);
+                    if (footerTranslation > footerHeight) {
+                        footerTranslation = footerHeight;
+                    }
+
+                    LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mFooterView.getLayoutParams();
+                    if (lp.bottomMargin == -footerHeight) {
+                        mFooterView.setVisibility(View.GONE);
+                    }
+                    if (footerTranslation < footerHeight && mFooterView.getVisibility() == View.GONE){
+                        mFooterView.setVisibility(View.VISIBLE);
+                    }
+                    if (mFooterView.getVisibility() == View.VISIBLE){
+                        lp.bottomMargin = -footerTranslation;
+                        mFooterView.setLayoutParams(lp);
+                        mViewPagerRoot.invalidate();
+                    }
+
+                }
+            }
         }
 
         @Override
         public void onPageSelected(int position) {
-            TabHelpFragment selected = mAdapter.getItem(position);
-            mHelpText.setText(selected.getContentText());
+            if (position < positionFeedback) {
+                BaseTabFragment selected = mAdapter.getItem(position);
+                mHelpText.setText(selected.getContentText());
+            }
         }
 
         @Override
